7ecff557a52719696c99ab1c95e5f13c486ea007
mario38lg23 <mariolopezgas@gmail.com>
2025-03-26 21:26:36 +0100

    avance3

--- src/XML.java
+++ src/XML.java
@@ -1,4 +1,9 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class XML {
     private List<Map<String, String>> elementos;
@@ -29,10 +34,39 @@ public class XML {
         nombreRaiz = null;
     }
 
-    public List<Map<String, String>> leerXML(String rutaArchivo) {
-        return null;
+    public void leerArchivoXML(String ruta) {
+        System.out.println("La ruta seleccionada es un archivo XML.");
+
+        try (BufferedReader br = new BufferedReader(new FileReader(ruta))) {
+            StringBuilder contenidoXML = new StringBuilder();
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                contenidoXML.append(linea.trim());
+            }
+
+            String xml = contenidoXML.toString();
+
+            
+            Pattern etiquetaPattern = Pattern.compile("<([^/][^>]*)>(.*?)</\\1>");
+            Matcher etiquetaMatcher = etiquetaPattern.matcher(xml);
+
+            while (etiquetaMatcher.find()) {
+                String clave = etiquetaMatcher.group(1).trim();
+                String valor = etiquetaMatcher.group(2).trim();
+
+                
+                Map<String, String> mapaEtiqueta = new HashMap<>();
+                mapaEtiqueta.put(clave, valor);
+
+                
+                elementos.add(mapaEtiqueta);
+            }
+        } catch (IOException e) {
+            System.out.println("Error al leer el archivo: " + e.getMessage());
+        }
     }
 
     public void escribirXML(String rutaArchivo) {
+
     }
 }
